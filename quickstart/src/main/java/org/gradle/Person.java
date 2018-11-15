7cb754afd0bf1edb0ee27e78b4065bd7229d9b94
cesar1414 <ccalvilujan@gmail.com>
2018-11-15 09:05:54 -0400

    task #8

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -3,14 +3,40 @@ package org.gradle;
 import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
-    private final String name;
+
+    private final String names;
+    private final String lastames;
+    private final int age;
+
+    public Person(String name, String lastnames, int age) {
+        this.names = name;
+        this.lastames = lastnames;
+        this.age = age;
+    }
+
 
     public Person(String name) {
-        this.name = name;
-        new GrowthList();
+        this.names = name;
+        this.lastames = "";
+        this.age = 0;
+    }
+
+    public Person() {
+        this.names = "";
+        this.lastames = "";
+        this.age = 0;
     }
 
     public String getName() {
-        return name;
+        return names;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastames() {
+        return lastames;
+    }
+
 }
